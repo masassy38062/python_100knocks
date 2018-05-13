8c84dcb1f934aea9d5054f420a1642155150568b
masassy38062 <fc3s.black1022@gmail.com>
2018-05-13 19:41:10 +0900

    026/100

---
+++ 026_emphasize_remove.py
@@ -0,0 +1,57 @@
+#26. 強調マークアップの除去
+#25の処理時に，テンプレートの値からMediaWikiの強調マークアップ
+# （弱い強調，強調，強い強調のすべて）を除去してテキストに変換せよ（参考: マークアップ早見表）．
+
+import json
+import gzip
+import re
+
+def uk_find():
+    basepath = './'
+    filename = 'jawiki-country.json.gz'
+    pattern = r"イギリス"
+    with gzip.open(basepath + filename, 'rt')as gf:
+        for line in gf:
+            # json.loadsはstr→dict、json.loadはfile→dict
+            json_data = json.loads(line)
+            if (re.match(json_data['title'], pattern)):
+                return json_data['text']
+
+def basic_info_find(lines):
+    pattern1 = re.compile(r'^\{\{[redirect|基礎情報].*')
+    pattern2 = re.compile(r'^\|.*')
+    pattern3 = re.compile(r'^\}\}$')
+    basic_dict = {}
+    for line in lines.split('\n'):
+        if pattern1.match(line):
+            continue
+
+        elif pattern2.match(line):
+            point = line.find('=')
+            MAX = len(line)
+            title = line[0:point].lstrip('|').rstrip(' ')
+            data = line[point:MAX].lstrip('= ')
+            basic_dict.update({title: data})
+
+        elif pattern3.match(line):
+            break
+    return basic_dict
+
+def emphasize_remove(basic_dict):
+    remove_dict={}
+    pattern = re.compile(r".*'{2,4}.*")
+    for key,value in basic_dict.items():
+        if pattern.match(value):
+            value = value.replace("\'",'')
+            remove_dict.update({key:value})
+    return remove_dict
+
+if __name__ == "__main__":
+    lines = uk_find()
+    basic_dict = basic_info_find(lines)
+    emphasize_remove_dict = emphasize_remove(basic_dict)
+
+    for key,value in emphasize_remove_dict.items():
+        print(key+':'+value)
+
+    print("データ数",len(emphasize_remove_dict.items()))
