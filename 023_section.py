9b733865c8ab307b5a79b5d8226ec4ced2a04eb9
masassy38062 <fc3s.black1022@gmail.com>
2018-05-13 19:24:59 +0900

    008/100

---
+++ 023_section.py
@@ -0,0 +1,35 @@
+#23. セクション構造
+#記事中に含まれるセクション名とそのレベル（例えば"== セクション名 =="なら1）を表示せよ．
+
+import json
+import gzip
+import re
+
+def uk_find():
+    basepath = './'
+    filename = 'jawiki-country.json.gz'
+    pattern = r"イギリス"
+    with gzip.open(basepath + filename, 'rt')as gf:
+        for line in gf:
+            # json.loadsはstr→dict、json.loadはfile→dict
+            json_data = json.loads(line)
+            if (re.match(json_data['title'], pattern)):
+                return json_data['text']
+
+if __name__=="__main__":
+    pattern = re.compile(r'^=.*')
+    pattern2 = re.compile(r'^={2}')
+    pattern3 = re.compile(r'^={3}')
+    pattern4 = re.compile(r'^={4}')
+
+    lines=uk_find()
+    for line in lines.split('\n'):
+        if pattern.match(line):
+            if pattern4.match(line):
+                print(line.lstrip('====').rstrip('====')+':レベル4')
+            elif pattern3.match(line):
+                print(line.lstrip('===').rstrip('===')+':レベル3')
+            elif pattern2.match(line):
+                print(line.lstrip('==').rstrip('==')+':レベル2')
+            else:
+                print('no match')
